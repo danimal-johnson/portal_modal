de96366667a70ce057688b37f0e71ee137c60f71
Dan Johnson <danimal.johnson@gmail.com>
2020-08-10 08:00:37 -0700

    Create modal using ReactDom portal. Very basic.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Modal from './Modal'
+
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1
+}
+
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  PADDING: '10px'
+}
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={BUTTON_WRAPPER_STYLES}>
+        <button onClick={()=>{setIsOpen(true)}}>Open Modal</button>
+        <Modal open={isOpen} onClose={()=>setIsOpen(false)}>
+          Fancy Modal
+        </Modal>
+      </div>
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
     </div>
   );
 }
